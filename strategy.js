0c48f01823bd6f97c6e42a59464504b1f18be483
Jordan Porter <jporter@ngacres.com>
2018-02-24 09:43:09 -0700

    hide string

--- strategy.js
+++ strategy.js
@@ -1,8 +1,6 @@
 const Auth0Strategy = require('passport-auth0');
 const config = require(`${__dirname}/config.js`);
 const { domain, clientID, clientSecret } = config;
-// const massive = require('massive');
-// const massiveInstance = massive.connectSync({connectionString: "postgres://qtsfzuurclzgcr:a8f2fe8717e00e1983e95c82fadb389cc7ece9a4f6d13c97634184322412795c@ec2-184-72-219-186.compute-1.amazonaws.com:5432/d5shi0ki18lf3s?ssl=true"})
 
 module.exports = new Auth0Strategy({
     domain: domain,
